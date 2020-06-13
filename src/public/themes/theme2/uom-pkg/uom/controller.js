b2af0b67ebe3e6137b0f0eaa5184c928fad74289
Parthi <parthiban@uitouxsolutions.com>
2020-06-13 15:18:21 +0530

    change error noty

--- src/public/themes/theme2/uom-pkg/uom/controller.js
+++ src/public/themes/theme2/uom-pkg/uom/controller.js
@@ -237,11 +237,7 @@ app.component('uomForm', {
                         } else {
                             if (!res.success == true) {
                                 $('.submit').button('reset');
-                                var errors = '';
-                                for (var i in res.errors) {
-                                    errors += '<li>' + res.errors[i] + '</li>';
-                                }
-                                custom_noty('error', errors);
+                                showErrorNoty(res);
                             } else {
                                 $('.submit').button('reset');
                                 $location.path('/uom-pkg/uom/list');
